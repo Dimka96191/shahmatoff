3fa06759c9f9cb1353b9a008b36cca4c6babffc4
Evgeniy Datskij <evgenij.datskij@mail.ru>
2023-02-05 01:16:12 +0500

    установил библиотечку, потому что тот парсер который я писал не проходит cors политику....

--- app/js/parser.js
+++ app/js/parser.js
@@ -1,12 +1,32 @@
 window.addEventListener("DOMContentLoaded", () => {
   const body = document.querySelector("body");
+
+  let textNodes = [];
+
+  // recurcy функция которая проходится по всем элементам страницы, и по всем вложенностям, до тех пор пока не найдет конечный элемент
+
   function recurcy(element) {
     element.childNodes.forEach((node) => {
-      console.log(node);
-      if (element.childNodes.lenght > 1) {
+      if (node.nodeName.match(/^H\d/)) {
+        const obj = {
+          header: node.nodeName,
+          content: node.textContent,
+        };
+        textNodes.push(obj);
+      } else {
         recurcy(node);
       }
     });
   }
   recurcy(body);
+
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(textNodes),
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
 });
