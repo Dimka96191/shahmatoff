cb1f760fd15c36703dd9c9b554a388bb0571819e
Dimka96191 <Dim.pupkov@yandex.ru>
2023-02-06 16:03:26 +0500

    закончил страницу КОНТАКТЫ

--- app/js/main.js
+++ app/js/main.js
@@ -10,7 +10,7 @@ function burgerMenu() {
       body.classList.add('locked')
     } else {
       menu.classList.remove('active')
-      burger.classList.remove('active') 
+      burger.classList.remove('active')
       body.classList.remove('locked')
     }
   })
@@ -24,3 +24,36 @@ function burgerMenu() {
   })
 }
 burgerMenu()
+
+
+// Яндекс карта 
+
+ymaps.ready(init);
+function init() {
+  var myMap = new ymaps.Map("map", {
+    center: [56.82, 60.60],
+    zoom: 13,
+    controls: ['smallMapDefaultSet']
+  }, {
+    restrictMapArea: [
+      [56.699, 60.312],
+      [56.989, 60.880]
+    ]
+  }),
+    myGeoObject = new ymaps.GeoObject()
+
+
+  myMap.geoObjects
+    .add(new ymaps.Placemark([56.830668, 60.592948], {
+      balloonContent: 'Хохрякова, 48'
+    }),
+    );
+
+  myMap.geoObjects
+    .add(new ymaps.Placemark([56.802107, 60.602072], {
+      balloonContent: 'Авиационная улица, 10'
+    }),
+    );
+
+}
+
