8bbaf11cfa02e9f875ff4b1b142118d5d7bde15e
Dimka96191 <Dim.pupkov@yandex.ru>
2023-02-07 19:51:37 +0500

    фикс паралакс и создал map.js

--- app/js/main.js
+++ app/js/main.js
@@ -26,34 +26,10 @@ function burgerMenu() {
 burgerMenu()
 
 
-// Яндекс карта 
+// import "../css/style.min.css";
 
-ymaps.ready(init);
-function init() {
-  var myMap = new ymaps.Map("map", {
-    center: [56.82, 60.60],
-    zoom: 13,
-    controls: ['smallMapDefaultSet']
-  }, {
-    restrictMapArea: [
-      [56.699, 60.312],
-      [56.989, 60.880]
-    ]
-  }),
-    myGeoObject = new ymaps.GeoObject()
-
-
-  myMap.geoObjects
-    .add(new ymaps.Placemark([56.830668, 60.592948], {
-      balloonContent: 'Хохрякова, 48'
-    }),
-    );
-
-  myMap.geoObjects
-    .add(new ymaps.Placemark([56.802107, 60.602072], {
-      balloonContent: 'Авиационная улица, 10'
-    }),
-    );
-
-}
+// parallax
+window.addEventListener("scroll", () => {
+  document.body.style.cssText = `--scrollTop: ${window.scrollY}px`;
+});
 
