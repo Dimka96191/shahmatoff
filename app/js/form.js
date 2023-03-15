3ee9cc22c7457e8ddaead846d9c9050d9735d282
Evgeniy Datskij <evgenij.datskij@mail.ru>
2023-03-15 23:59:29 +0500

    xz

--- app/js/form.js
+++ app/js/form.js
@@ -1,70 +1,69 @@
-"use strick"
+"use strick";
 
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById('form');
-    form.addEventListener('submit', formSend);
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("form");
+  form.addEventListener("submit", formSend);
 
-    async function formSend(e) {
-        e.preventDefault();
+  async function formSend(e) {
+    e.preventDefault();
 
-        let error = formValidate(form);
+    let error = formValidate(form);
 
+    let formDate = new FormData(form);
 
-        let formDate = new FormData(form);
+    if (error === 0) {
+      //   form.classList.add("_sending");
 
+      console.log(formDate);
+      // let response = await fetch('../sendmail.php', {
+      //     method: 'post',
+      //     body: formDate
+      // });
+      // if (response.ok) {
+      //     let result = await response.json();
+      //     alert(result.message);
+      //     formPreview.innerHTML = '';
+      //     form.reset();
+      //     form.classList.remove('_sending');
+      // } else {
+      //     alert("Ошибка");
+      //     form.classList.remove('_sending');
+      // }
+    } else {
+      alert("Заполните обязательные поля");
+    }
+  }
+  function formValidate(form) {
+    let error = 0;
+    let formReq = document.querySelectorAll("._req");
 
-        if (error === 0) {
-            form.classList.add('_sending');
-            let response = await fetch('../sendmail.php', {
-                method: 'post',
-                body: formDate
-            });
-            if (response.ok) {
-                let result = await response.json();
-                alert(result.message);
-                formPreview.innerHTML = '';
-                form.reset();
-                form.classList.remove('_sending');
-            } else {
-                alert("Ошибка");
-                form.classList.remove('_sending');
-            }
-        } else {
-            alert('Заполните обязательные поля')
+    for (let index = 0; index < formReq.length; index++) {
+      const input = formReq[index];
+      formRemoveError(input);
+
+      if (
+        input.getAttribute("type") === "checkbox" &&
+        input.checked === false
+      ) {
+        formAddError(input);
+        error++;
+      } else {
+        if (input.value === "") {
+          formAddError(input);
+          error++;
         }
+      }
     }
-    function formValidate(form) {
-        let error = 0;
-        let formReq = document.querySelectorAll('._req');
+    return error;
+  }
 
-        for (let index = 0; index < formReq.length; index++) {
-            const input = formReq[index];
-            formRemoveError(input);
+  function formAddError(input) {
+    input.parentElement.classList.add("_error");
+    input.classList.add("_error");
+  }
 
-            if (input.getAttribute("type") === "checkbox" && input.checked === false) {
-                formAddError(input);
-                error++;
-            } else {
-                if (input.value === '') {
-                    formAddError(input);
-                    error++;
-                }
-            }
-        }
-        return error;
-    }
-
-    function formAddError(input) {
-        input.parentElement.classList.add('_error');
-        input.classList.add('_error');
-    }
-
-    function formRemoveError(input) {
-        input.parentElement.classList.remove('_error');
-        input.classList.remove('_error');
-    }
-
-
+  function formRemoveError(input) {
+    input.parentElement.classList.remove("_error");
+    input.classList.remove("_error");
+  }
 });
-
-
