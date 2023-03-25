e9c3130cff9b4f4f70be0ba210a3648548eb628b
Dimka96191 <Dim.pupkov@yandex.ru>
2023-03-25 13:52:11 +0500

    add map in footer

--- app/js/main.js
+++ app/js/main.js
@@ -157,3 +157,38 @@ akkardion();
 //           elm.off("click");
 //       });
 // });
+
+
+// Яндекс карта 
+
+ymaps.ready(init);
+function init() {
+  var myMap = new ymaps.Map("mapFooter", {
+    center: [56.82, 60.60],
+    zoom: 11,
+    controls: ['smallMapDefaultSet']
+  }, {
+    restrictMapArea: [
+      [56.699, 60.312],
+      [56.989, 60.880]
+    ]
+  }),
+    myGeoObject = new ymaps.GeoObject()
+
+  myMap.behaviors
+    .disable('scrollZoom');
+
+
+  myMap.geoObjects
+    .add(new ymaps.Placemark([56.830668, 60.592948], {
+      balloonContent: 'Хохрякова, 48'
+    }),
+    );
+
+  myMap.geoObjects
+    .add(new ymaps.Placemark([56.802107, 60.602072], {
+      balloonContent: 'Авиационная улица, 10'
+    }),
+    );
+
+}
